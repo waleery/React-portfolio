1553fd0bd72fa2df6b3422abf434882d5555628e
waleery <walenda1997@gmail.com>
2022-05-11 10:48:03 +0200

    Headline and button 'contact me'

---
+++ src/components/Home/index.js
@@ -0,0 +1,24 @@
+import LogoTitle from '../../assets/images/logo-s.png'
+import { Link } from 'react-router-dom'
+import './index.scss'
+const Home = () => {
+    return (
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1>
+                    Hi, <br /> I'm
+                    <img src={LogoTitle} alt="developer" />
+                    lobodan
+                    <br />
+                    web developer
+                </h1>
+                <h2>Student / Trying to be Frontend Developer</h2>
+                <Link to="/contact" className="flat-button">
+                    CONTACT ME
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default Home
