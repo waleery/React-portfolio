ba47d99212853d21722b948f36bb7170daee9235
waleery <walenda1997@gmail.com>
2022-05-12 08:01:40 +0200

    Add bouncing letters

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,16 +1,56 @@
 import LogoTitle from '../../assets/images/logo-s.png'
 import { Link } from 'react-router-dom'
 import './index.scss'
+import { useState } from 'react'
+import AnimatedLetters from '../AnimatedLetters'
 const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const hiArray = ['H', 'i', ',']
+    const imArray = ['I', "'", 'm']
+    const nameArray = ['a', 'l', 'e', 'r', 'y']
+    const jobArray = [
+        'w',
+        'e',
+        'b',
+        ' ',
+        'd',
+        'e',
+        'v',
+        'e',
+        'l',
+        'o',
+        'p',
+        'e',
+        'r',
+        '.',
+    ]
     return (
         <div className="container home-page">
             <div className="text-zone">
                 <h1>
-                    Hi, <br /> I'm
-                    <img src={LogoTitle} alt="developer" />
-                    lobodan
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={hiArray}
+                        idx={10}
+                    />
                     <br />
-                    web developer
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={imArray}
+                        idx={14}
+                    />
+                    <img src={LogoTitle} alt="developer" />
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={nameArray}
+                        idx={17}
+                    />
+                    <br />
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={jobArray}
+                        idx={22}
+                    />
                 </h1>
                 <h2>Student / Trying to be Frontend Developer</h2>
                 <Link to="/contact" className="flat-button">
