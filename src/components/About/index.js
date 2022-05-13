6944e709803dc9acaf69279e157fb1bbc430f2e9
waleery <walenda1997@gmail.com>
2022-05-13 21:24:23 +0200

    Spinnig cube (about me)

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,11 +1,19 @@
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
 import { useEffect, useState } from 'react'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+    faCss3,
+    faGitAlt,
+    faGithub,
+    faGithubAlt,
+    faHtml5,
+    faJava,
+    faJsSquare,
+    faReact,
+} from '@fortawesome/free-brands-svg-icons'
 const About = () => {
-
     const [letterClass, setLetterClass] = useState('text-animate')
-
 
     useEffect(() => {
         setTimeout(() => {
@@ -38,7 +46,30 @@ const About = () => {
                     fanatic, photography enthusiast, and tech-obsessed!!!
                 </p>
             </div>
-            
+
+            <div className="stage-cube-cont">
+                <div className='center'>X</div>
+                <div className="cubespinner">
+                    <div className="face1">
+                        <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+                    </div>
+                    <div className="face2">
+                        <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+                    </div>
+                    <div className="face3">
+                        <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+                    </div>
+                    <div className="face4">
+                        <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+                    </div>
+                    <div className="face5">
+                        <FontAwesomeIcon icon={faJava} color="#333333" />
+                    </div>
+                    <div className="face6">
+                        <FontAwesomeIcon icon={faGitAlt} color="#EC4D2A" />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
