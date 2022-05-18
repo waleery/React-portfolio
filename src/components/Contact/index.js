70e7d14adf3392b19bf757a5785cbd7c3715f2dd
waleery <walenda1997@gmail.com>
2022-05-18 08:24:06 +0200

    working EmailJs

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,16 +1,38 @@
 import Loader from 'react-loaders'
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import emailjs from '@emailjs/browser';
+import { faL } from '@fortawesome/free-solid-svg-icons'
 
 const Contact = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
+    const refForm = useRef()
 
     useEffect(() => {
         setTimeout(() => {
             setLetterClass('text-animate-hover')
         }, 3000)
     }, [])
+
+    const sendEmail = (e) => {
+        e.preventDefault()
+
+        emailjs.sendForm(
+            'service_q3qt8uf',
+            'template_7br2n8j',
+            refForm.current,
+            'jtf5GKdOr9tYibcFd'
+        ).then(
+            () => {
+                alert('Message successfully sent!')
+                window.location.reload(false)
+            },
+            () => {
+                alert('Failed to send, try again')
+            }
+        )
+    }
 
     return (
         <div>
@@ -41,7 +63,7 @@ const Contact = () => {
                         below form either.
                     </p>
                     <div className="contact-form">
-                        <form>
+                        <form ref={refForm} onSubmit={sendEmail}>
                             <ul>
                                 <li className="half">
                                     <input
@@ -75,7 +97,11 @@ const Contact = () => {
                                     ></textarea>
                                 </li>
                                 <li>
-                                    <input type="submit" className='flat-button' value="SEND" />
+                                    <input
+                                        type="submit"
+                                        className="flat-button"
+                                        value="SEND"
+                                    />
                                 </li>
                             </ul>
                         </form>
