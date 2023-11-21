923cb3dfeeebbfb1614ef07335d46e13ac8cd47f
waleery <walenda1997@gmail.com>
2023-11-21 18:04:14 +0100

    Addaptation 'about' and 'contact' to small devices

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,8 +2,8 @@ import Loader from 'react-loaders'
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
 import { useEffect, useRef, useState } from 'react'
-import emailjs from '@emailjs/browser';
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import emailjs from '@emailjs/browser'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 
 const Contact = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
@@ -18,25 +18,27 @@ const Contact = () => {
     const sendEmail = (e) => {
         e.preventDefault()
 
-        emailjs.sendForm(
-            'service_q3qt8uf',
-            'template_7br2n8j',
-            refForm.current,
-            'jtf5GKdOr9tYibcFd'
-        ).then(
-            () => {
-                alert('Message successfully sent!')
-                window.location.reload(true);
-            },
-            () => {
-                alert('Failed to send, try again')
-            }
-        )
+        emailjs
+            .sendForm(
+                'service_q3qt8uf',
+                'template_7br2n8j',
+                refForm.current,
+                'jtf5GKdOr9tYibcFd'
+            )
+            .then(
+                () => {
+                    alert('Message successfully sent!')
+                    window.location.reload(true)
+                },
+                () => {
+                    alert('Failed to send, try again')
+                }
+            )
     }
 
     return (
         <>
-            <div className="container contact-page">
+            <div className="contact-page">
                 <div className="text-zone">
                     <h1>
                         <AnimatedLetters
@@ -57,10 +59,10 @@ const Contact = () => {
                         />
                     </h1>
                     <p>
-                        I am interested in an internship - especially
-                        ambitious or large projects. However, if you have other
-                        request or question, don't hesitate to contact me using
-                        below form either.
+                        I am interested in an internship - especially ambitious
+                        or large projects. However, if you have other request or
+                        question, don't hesitate to contact me using below form
+                        either.
                     </p>
                     <div className="contact-form">
                         <form ref={refForm} onSubmit={sendEmail}>
@@ -107,24 +109,30 @@ const Contact = () => {
                         </form>
                     </div>
                 </div>
-            </div>
-            <div className='info-map'>
-                Patryk Walendziuk,
-                <br/>
-                Poland,
-                <br/>
-                Bialystok
-                <span>p.k.walendziuk@gmail.com</span>
+                <div className="map-wrap">
+                    <MapContainer
+                        center={[53.13212150415938, 23.16846514114768]}
+                        zoom={13}
+                    >
+                        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                        <Marker
+                            position={[53.13212150415938, 23.16846514114768]}
+                        >
+                            <Popup>Hey, I live here :)</Popup>
+                        </Marker>
+                    </MapContainer>
 
+                    <div className="info-map">
+                        Patryk Walendziuk,
+                        <br />
+                        Poland,
+                        <br />
+                        Bialystok
+                        <span>p.k.walendziuk@gmail.com</span>
+                    </div>
+                </div>
             </div>
-            <div className='map-wrap'>
-                <MapContainer center={[53.13212150415938, 23.16846514114768]} zoom={13}>
-                    <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-                    <Marker position={[53.13212150415938, 23.16846514114768]}>
-                        <Popup>Hey, I live here :)</Popup>
-                    </Marker>
-                </MapContainer>
-            </div>
+
             <Loader type="pacman" />
         </>
     )
